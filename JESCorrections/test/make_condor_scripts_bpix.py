a30e2a2a953bd0f40e13f537978e02ca329a22f1
blehtela <142437787+blehtela@users.noreply.github.com>
2025-01-07 14:38:11 +0100

    Changes to creation of condor scripts. Log file names will also contain bpix category. Settings as used for my tests on 21.11.2024. Will update now to make separate .htc scripts for each category.

--- JESCorrections/test/make_condor_scripts_bpix.py
+++ JESCorrections/test/make_condor_scripts_bpix.py
@@ -1,16 +1,19 @@
 import os
 ############# USER DEFINED ##################################################
 # Directory with input files
-input_directory = "/eos/user/t/tchatzis/JRA_NTuples_Winter24"
+#input_directory = "/eos/user/t/tchatzis/JRA_NTuples_Winter24"
+input_directory = "/eos/user/b/blehtela/JRA_NTuples_Winter24"
 
-bpix_categories = ['noBPix','BPix','BPixPlus','BPixMinus']
+#bpix_categories = ['noBPix','BPix','BPixPlus','BPixMinus']
+bpix_categories = ['noBPix','BPix'] #try with only two now, maybe jobs ran too long
 flatPU_label = 'FlatPU0to80'
 noPU_label = 'NoPU'
 
 #############################################################################
 
 # Loop over each input file
-with open("sub_jecs_total_forBPix.htc", "w") as job_script:
+#with open("sub_jecs_total_forBPix.htc", "w") as job_script:
+with open("sub_jecs_test_07jan_bettina.htc", "w") as job_script: #different name for my testing phase
     # Common parts in all jobs
     job_script.write("executable            = fitJESCs\n")
     job_script.write("getenv                = True\n")
@@ -24,16 +27,20 @@ with open("sub_jecs_total_forBPix.htc", "w") as job_script:
     for bpix_cat in bpix_categories:
         job_script.write("\n"*5)
         job_script.write(f"transfer_output_files = JESCs_2024_{bpix_cat}\n")
-        jet_categories = ['ak4pfHLT','ak8pfHLT']
+        #jet_categories = ['ak4pfHLT','ak8pfHLT']
+        jet_categories = ['ak4pfHLT'] #try only ak4 to see if it does all steps then
         if bpix_cat == 'noBPix':
-            jet_categories = ['ak4pfHLT','ak8pfHLT','ak4caloHLT','ak8caloHLT']
+            #jet_categories = ['ak4pfHLT','ak8pfHLT','ak4caloHLT','ak8caloHLT']
+            jet_categories = ['ak4pfHLT','ak4caloHLT'] #only ak4 (see above)
         for jet_cat in jet_categories:
             job_script.write("\n"*5)
             job_script.write(f"arguments             =  -i_nopu {input_directory}/JRA_{noPU_label}{bpix_cat}.root -i_flatpu {input_directory}/JRA_{flatPU_label}{bpix_cat}.root -o JESCs_2024_{bpix_cat} -b -j {jet_cat} -n 1000000\n")
-            job_script.write(f"output = htc_out_2024/{jet_cat}.out\n")
-            job_script.write(f"error = htc_out_2024/{jet_cat}.err\n")
-            job_script.write(f"log = htc_out_2024/{jet_cat}.log\n")
-            job_script.write("+JobFlavour           = \"workday\"\n")
+            job_script.write(f"output = htc_out_2024/{bpix_cat}_{jet_cat}.out\n")
+            job_script.write(f"error = htc_out_2024/{bpix_cat}_{jet_cat}.err\n")
+            job_script.write(f"log = htc_out_2024/{bpix_cat}_{jet_cat}.log\n")
+            #job_script.write("+JobFlavour           = \"workday\"\n")
+            #job_script.write("+JobFlavour           = \"tomorrow\"\n") #try a bit longer in case jobs run too long
+            job_script.write("+JobFlavour           = \"testmatch\"\n") #try a even longer
             job_script.write("queue\n")
 
 
