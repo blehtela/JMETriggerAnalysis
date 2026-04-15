4f6f404409425181ec5e3b7533bcb4b2427de285
blehtela <142437787+blehtela@users.noreply.github.com>
2026-04-15 19:03:26 +0200

    updated the command for getting the current menu (status 3rd of March 2026) in the bash script, where i get only selected paths that we need - need to update this before next usage.

--- Common/test/dumpHLTMenus_dataRun3_Feb2026_selectedPaths.sh
+++ Common/test/dumpHLTMenus_dataRun3_Feb2026_selectedPaths.sh
@@ -2,16 +2,22 @@
 
 # Taken from SWGuideGlobalHLT page : https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideGlobalHLT
 #hltGetConfiguration /dev/CMSSW_16_0_0/GRun \
-hltGetConfiguration /dev/CMSSW_16_0_0/GRun/V2 \
-   --globaltag 151X_dataRun3_HLT_v1 \
+#hltGetConfiguration /dev/CMSSW_16_0_0/GRun/V2 \
+hltGetConfiguration /dev/CMSSW_16_0_0/GRun/V30 \
+   --globaltag 160X_dataRun3_HLT_v1 \
    --data \
    --unprescale \
    --output minimal \
    --max-events 100 \
-   --eras Run3_2025 --l1-emulator uGT --l1 L1Menu_Collisions2025_v1_3_0_xml \
+   --eras Run3_2026 --l1-emulator uGT --l1 L1Menu_Collisions2026_v1_0_0_xml \
    --path MC_*,HLTriggerF*,Status*,HLT_PFJet60_v*,HLT_PFJet140_v*,HLT_PFJet320_v*,HLT_PFJet500_v*,HLT_PFHT780_v*,HLT_PFHT890_v*,HLT_PFHT1050_v*,HLT_PFMET*_PFMHT*_IDTight_v*,HLT_IsoMu27*,HLT_METnoMu*,HLT_PFJet40_GPUvsCPU_v* \
    --input /store/data/Run2025G/EphemeralHLTPhysics0/RAW/v1/000/398/183/00000/002bbd0c-b9ed-4758-b7a6-e2e13149ca34.root \
    > hltData_tmp.py
+
+# copies of only the lines that i updated in the command above (on 03.03.2026)
+#hltGetConfiguration /dev/CMSSW_16_0_0/GRun/V2 \
+#--globaltag 151X_dataRun3_HLT_v1 \
+#--eras Run3_2025 --l1-emulator uGT --l1 L1Menu_Collisions2025_v1_3_0_xml \
 
 
 # dump configuration (note: CMSSW_16_0_0_GRun)
